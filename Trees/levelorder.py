138067ac84bd1f74b3faafb99471ac3ad02354af
peninahnaserian <peshnase@gmail.com>
2023-11-23 12:28:15 +0300

    LevelOrderTraversal

---
+++ Trees/levelorder.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+                
+                
+def level_order(root:Node):
+    
+    if root is None:
+        return []
+    
+    
+    queue = [root]
+    next_queue = []
+    level = []
+    result = []
+        
+    while queue != []:
+        for root in queue:
+            level.append(root.info)
+            if root.left is not None:
+                next_queue.append(root.left)
+            if root.right is not None:
+                next_queue.append(root.right)
+            
+        result.append(level)
+        level = []
+        queue = next_queue
+        next_queue = []            
+    
+    return result
+
+#code runner
+root = Node(1)
+root.left = Node(2)
+root.right = Node(3)
+root.left.left = Node(4)
+root.left.right = Node(5)
+
+print(level_order(root))
