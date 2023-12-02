4767a464a1f7437e30d264ba292517f42b334227
peninahnaserian <peshnase@gmail.com>
2023-12-02 23:14:30 +0300

    Trees

---
+++ Trees/search.py
@@ -0,0 +1,13 @@
+  def searchBST(root, val):
+        stack, subtree = [], None
+        while True:
+            while root:
+                if root.val == val: subtree = root
+                stack.append(root)
+                root = root.left
+
+            if subtree: return subtree
+            
+            if not stack: return None
+
+            root = stack.pop().right
