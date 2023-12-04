bab495dcd5083ea092b8aaadd89f2f7d17a91f50
peninahnaserian <peshnase@gmail.com>
2023-12-04 18:40:32 +0300

    Trees

---
+++ Trees/sametree.py
@@ -0,0 +1,16 @@
+def is_same_tree(p, q):
+    
+    def dfs(node1, node2):
+
+        if node1 is None and node2 is None: #reach at the end of the trees and all matched
+            return True
+
+        elif node1 is None or node2 is None: #only one of the two is present
+            return False
+
+        elif node1.val != node2.val:  #if the values don't match
+            return False
+
+        return dfs(node1.left, node2.left) and dfs(node1.right, node2.right)
+    
+    return dfs(p, q)
